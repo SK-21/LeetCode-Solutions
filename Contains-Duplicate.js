b0815200e38ba8e6d4482ffb1613bc3977f0cc9a
Sapna Kashyap <sapnakashyap2109@gmail.com>
2023-06-04 22:32:41 +0530

    solution

---
+++ Contains-Duplicate.js
@@ -0,0 +1,15 @@
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    //check duplication
+ nums.sort(); //first sorting the array
+    for( let i=0;i<nums.length;i++){
+        if(nums[i] === nums[i+1]){
+            return true;
+        } 
+    }
+        return false     
+};
